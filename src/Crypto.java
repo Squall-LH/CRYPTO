c5e0901832cf01a60f33d3dc5040074784fbb37d
Squall-LH <regnak90@gmail.com>
2012-11-19 01:29:57 +0100

    Implement chiffre method in Permutation class.

--- src/Crypto.java
+++ src/Crypto.java
@@ -5,16 +5,16 @@ public class Crypto
 		String message = "RAYMOND QUENEAU EST UN AUTEUR FANTASTIQUE";
 		
 		//message
-		System.out.println(message);
+		System.out.println("Message clair: " + message);
 		
 		String cle = "ECRITURE";
-		
+		System.out.println("Clé: " + cle);
 		//message chiffré
 		String messageChiffre = Permutation.chiffrer(cle,message);
-		System.out.println(messageChiffre);
+		System.out.println("Message chiffré:\n" + messageChiffre);
 		
 		//message déchiffré
-		String messageDechiffre = Permutation.dechiffrer(cle,messageChiffre);
-		System.out.println(messageDechiffre);
+		//String messageDechiffre = Permutation.dechiffrer(cle,messageChiffre);
+		//System.out.println(messageDechiffre);
 	}
 }
--- src/Permutation.java
+++ src/Permutation.java
@@ -1,17 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Permutation
 {
-	//non opérationnel
+	public final static boolean printWorkSheet = true;
 	public static String chiffrer(String cle, String chaine)
 	{
-		int nbRows = chaine.length()/cle.length();
-		String tab[] = new String[cle.length()];
-		for (int i = 0; i < chaine.length(); i++)
-		{
-			tab[i%8] += chaine.charAt(i);
-		}
-		//ordre à déterminer selon la clé
-		String chiffre = tab[1]+tab[0]+tab[7]+tab[3]+tab[2]+tab[6]+tab[4]+tab[5];
-		return(chiffre);
+        List<String> workSheet = new ArrayList<String>();
+        StringBuffer ciphered = new StringBuffer();
+        StringBuffer messageBuffer = new StringBuffer(chaine.replaceAll("\\s+", "").trim());
+        System.out.println("Message en clair sans espace: " + messageBuffer);
+        int keyLenght = cle.length();
+        List<Integer> keyInt = new ArrayList<Integer>();
+        
+        StringBuffer key = new StringBuffer(cle);
+        
+        for(int j = 0; j < key.length(); j++) {
+        	// index de la première lettre de key 
+        	Integer index = 1;
+        	char c  = key.charAt(j);
+        	for(int i = 0; i < key.length(); i++) {
+        		char c_next = key.charAt(i);
+        		if(c > c_next) {
+        			index++;
+        		}
+        	}
+        	while(keyInt.contains(index)) {
+        		index++;
+        	}
+        	keyInt.add(index);
+        }
+        
+        System.out.println("Clé numérique: " + keyInt.toString());
+        
+        while(messageBuffer.length() > 0) {
+        	String tmp;
+        	try{
+        		tmp = messageBuffer.substring(0, keyLenght);
+        	}
+        	catch(Exception e) {
+        		tmp = messageBuffer.toString();
+        		messageBuffer.setLength(0);
+        	}
+        	messageBuffer.delete(0, keyLenght);
+        	if(printWorkSheet) {
+        		System.out.println("workSheet: " + tmp);
+        	}
+        	workSheet.add(tmp.toString());
+        }
+        
+        for(int i = 1; i < keyInt.size()+1; i++) {
+        	int index = keyInt.indexOf(i);
+        	StringBuffer column = new StringBuffer();
+        	for(String line : workSheet) {
+        		if(line.length() > index)
+        			column.append(line.charAt(index));
+        	}
+        	ciphered.append(column);
+        }
+        
+        return ciphered.toString();
 	}
 	
 	public static String dechiffrer(String cle, String chaine)
