6d408a16b8bda5ac2011dd14ccea23cee08dcc4e
= <flolep@mailoo.org>
2012-11-30 16:19:37 +0100

    frequences (caractère, bigramme, trigramme) depuis un fichier texte

--- src/Crypto.java
+++ src/Crypto.java
@@ -1,23 +1,23 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
 public class Crypto
 {
 	public static void main(String[] args)
 	{
-		String message = "RAYMOND QUENEAU EST UN AUTEUR FANTASTIQUE";
-		
-		//message
-		System.out.println("Message clair: " + message);
-		
-		String cle = "ECRITURE";
-		System.out.println("Clé: " + cle);
-		//message chiffré
-		String messageChiffre = Transposition.chiffrer(cle,message);
-		System.out.println("Message chiffré:\n" + messageChiffre);
-		
-		//message déchiffré
-		//String messageDechiffre = Permutation.dechiffrer(cle,messageChiffre);
-		//System.out.println(messageDechiffre);
-		
-		String permut = Permutation.chiffrer(2,message);
-		System.out.println(permut+" -> "+Permutation.dechiffrer(2,permut));
+        //Scanner in = new Scanner(System.in);
+        //System.out.println("Veuillez saisir un alphabet :");
+        //String alphabet = in.next();
+
+        String fichier = "texte.txt";
+        
+        Texte t = new Texte(fichier);
+        Map<String,Double> list = t.frequences();
+       
+       for (Map.Entry<String,Double> entry : list.entrySet())
+       {
+           System.out.println(entry.getKey()+" :    "+entry.getValue());
+       }
 	}
 }
