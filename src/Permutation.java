6c3367340e2a10219c8478ce7b2bf83c056efbd9
Squall-LH <regnak90@gmail.com>
2012-12-02 18:55:41 +0100

    Add generate,cypher and decipher to Permutation class

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,23 +1,63 @@
-public class Permutation
-{
-	public static String chiffrer(int cle, String chaine)
-	{
-		String msg = "";
-		for (int i = 0; i < chaine.length(); i++)
-		{
-			int ascii = ((int) chaine.charAt(i));
-			msg += ((char) (ascii+cle));
-		}
-		return msg;
+import java.util.Random;
+
+public class Permutation {
+	public static char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+			'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+			'V', 'W', 'X', 'Y', 'Z' };
+
+	public static void main(String[] args) {
+		String key = generate(alphabet);
+		System.out.println("key: " + key);
+		String message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		System.out.println("message: " + message);
+		String cyphered = chiffrer(message, key);
+		System.out.println("cyphered: " + cyphered);
+		String clear = decipher(cyphered,key);
+		System.out.println("clear: " + clear);
 	}
-	
-	public static String dechiffrer(int cle, String chaine)
-	{
+
+	public static String generate(char[] alphabet) {
+		char[] key = alphabet;
+		Random r = new Random();
+
+		for (int i = 0; i < alphabet.length; i++) {
+			char tmp = key[i];
+			int rand = r.nextInt(alphabet.length);
+			key[i] = key[rand];
+			key[rand] = tmp;
+		}
+
+		System.out.println(key);
+		return String.copyValueOf(key);
+	}
+
+	public static String chiffrer(String message, String key) {
+		StringBuffer cyphered = new StringBuffer();
+
+		for (int i = 0; i < message.length(); i++) {
+			cyphered.append(key.charAt(message.charAt(i) - 65));
+		}
+
+		return cyphered.toString();
+	}
+
+	public static String decipher(String message, String key) {
+		StringBuffer cyphered = new StringBuffer();
+
+		for (int i = 0; i < message.length(); i++) {
+			int index = key.indexOf(message.charAt(i))+65;
+			char toAdd = (char)index;
+			cyphered.append(toAdd);
+		}
+
+		return cyphered.toString();
+	}
+
+	public static String dechiffrer(int cle, String chaine) {
 		String msg = "";
-		for (int i = 0; i < chaine.length(); i++)
-		{
+		for (int i = 0; i < chaine.length(); i++) {
 			int ascii = ((int) chaine.charAt(i));
-			msg += ((char) (ascii-cle));
+			msg += ((char) (ascii - cle));
 		}
 		return msg;
 	}
