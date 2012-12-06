ba810a256a22dc479f1c2ba5a565b13fe4a3a4f1
= <flolep@mailoo.org>
2012-12-06 10:40:33 +0100

    update Permutation

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,28 +1,26 @@
 import java.util.Random;
 
 public class Permutation {
-	public static char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-			'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-			'V', 'W', 'X', 'Y', 'Z' };
+	public static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	public static void main(String[] args) {
 		String key = generate(alphabet);
 		System.out.println("key: " + key);
-		String message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String message = "FUuU";
 		System.out.println("message: " + message);
-		String cyphered = chiffrer(message, key);
+		String cyphered = cypher(message, key, false);
 		System.out.println("cyphered: " + cyphered);
-		String clear = decipher(cyphered,key);
+		String clear = decipher(cyphered, key);
 		System.out.println("clear: " + clear);
 	}
 
-	public static String generate(char[] alphabet) {
-		char[] key = alphabet;
+	public static String generate(String alphabet) {
+		char[] key = alphabet.toCharArray();
 		Random r = new Random();
 
-		for (int i = 0; i < alphabet.length; i++) {
+		for (int i = 0; i < alphabet.length(); i++) {
 			char tmp = key[i];
-			int rand = r.nextInt(alphabet.length);
+			int rand = r.nextInt(alphabet.length());
 			key[i] = key[rand];
 			key[rand] = tmp;
 		}
@@ -31,11 +29,14 @@ public class Permutation {
 		return String.copyValueOf(key);
 	}
 
-	public static String chiffrer(String message, String key) {
+	public static String cypher(String message, String key, boolean delete) {
 		StringBuffer cyphered = new StringBuffer();
-
+		
 		for (int i = 0; i < message.length(); i++) {
-			cyphered.append(key.charAt(message.charAt(i) - 65));
+			if (key.indexOf(message.charAt(i)) >= 0)
+				cyphered.append(key.charAt(message.charAt(i) - 65));
+			else if (!delete)
+				cyphered.append('~');
 		}
 
 		return cyphered.toString();
@@ -45,14 +46,16 @@ public class Permutation {
 		StringBuffer cyphered = new StringBuffer();
 
 		for (int i = 0; i < message.length(); i++) {
-			int index = key.indexOf(message.charAt(i))+65;
-			char toAdd = (char)index;
+			int index = key.indexOf(message.charAt(i)) + 65;
+			char toAdd = '~';
+			if (message.charAt(i) != '~')
+				toAdd = (char) index;
 			cyphered.append(toAdd);
 		}
 
 		return cyphered.toString();
 	}
-
+/*
 	public static String dechiffrer(int cle, String chaine) {
 		String msg = "";
 		for (int i = 0; i < chaine.length(); i++) {
@@ -61,4 +64,5 @@ public class Permutation {
 		}
 		return msg;
 	}
+	*/
 }
