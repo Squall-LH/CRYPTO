8047ffd5166a5544efca7269300dcbf4d580895d
= <flolep@mailoo.org>
2013-01-07 20:41:22 +0100

    menu à compléter + fichier corpus.txt

--- src/Crypto.java
+++ src/Crypto.java
@@ -6,18 +6,34 @@ public class Crypto
 {
 	public static void main(String[] args)
 	{
-        //Scanner in = new Scanner(System.in);
-        //System.out.println("Veuillez saisir un alphabet :");
-        //String alphabet = in.next();
-
-        String fichier = "texte.txt";
-        
-        Texte t = new Texte(fichier);
-        Map<String,Double> list = t.frequences();
-       
-       for (Map.Entry<String,Double> entry : list.entrySet())
-       {
-           System.out.println(entry.getKey()+" :    "+entry.getValue());
-       }
+		//alphabet
+		Scanner in = new Scanner(System.in);
+		System.out.println("Veuillez saisir un alphabet\n('_' pour ABCDEFGHIJKLMNOPQRSTUVWXYZ) :");
+		String alphabet = in.next();
+		alphabet = alphabet.equals("_") ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : alphabet;
+		//crypto
+		in = new Scanner(System.in);
+		System.out.println("Type de chiffrement ?\n1) Permutation\n2) Vigenère");
+		String chiffrement = in.next();
+		//message
+		in = new Scanner(System.in);
+		System.out.println("Fichier texte à chiffrer :");
+		String fichier = in.next();
+		
+		Texte t = new Texte(fichier);
+		
+		if (chiffrement.equals("1"))
+		{
+			System.out.println("__Permutation__");
+			String key = Permutation.generate(alphabet);
+			System.out.println("key: " + key);
+			String cyphered = Permutation.cypher(t.getTexte(), key, false);
+			String cracked = Permutation.crack(cyphered, alphabet);
+			System.out.println("cracked: "+cracked);	
+		} else
+		{
+			System.out.println("__Vigenère__");
+		}
+		
 	}
 }
